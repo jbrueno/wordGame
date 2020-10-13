05eddb4e83a614d354fd388692975fc3ede88637
jbrueno <42968210+jbrueno@users.noreply.github.com>
2020-10-13 19:47:15 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,6 +3,9 @@ let NUM_LETTERS = 12; // 12 letters to start every game
 let string = ""; // will hold the letters that will be displayed
 let currentScore = 0;
 let prevGuesses = new Array();
+let startVal = 0; // when 0, startGame can be called, when 1, startGame can't be
+let seconds = 31; // number of seconds to play the game
+var timer;
 
 var firebaseConfig = {
     apiKey: "AIzaSyCF4S06cLFRsQh2drfEvm05pnoyDripvcE",
@@ -17,33 +20,72 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 let myDatabase = firebase.database();
-myDatabase.ref("dictionary").child("a").child("apple").once('value', ss=>{
-  alert(ss.val());
-})
 
-let randomIndex = parseInt(Math.floor(Math.random() * STARTERS));
-myDatabase.ref("starters").child("alphabetized").child(randomIndex).once('value').then(snapshot =>
+var $ = function( id ) { return document.getElementById( id ); };
+
+let startGame = function(){
+  if(startVal == 0){
+    $("guess").disabled = false;
+    loadletters();
+    seconds = 31;
+    timer = setInterval(setUpTimer, 1000);
+    startVal = 1;
+  }
+}
+
+let endGame = function(){
+  $('words').innerHTML = "";
+  $('letters').innerHTML = "";
+  $('score').innerHTML = "Score:  0";
+  $('guess').value = "";
+  $("over").innerHTML = "";
+  string = "";
+  prevGuesses = [];
+  currentScore = 0;
+  startVal = 0;
+  seconds = 31;
+  clearInterval(timer);
+}
+
+let loadletters = function(){
+  let randomIndex = parseInt(Math.floor(Math.random() * STARTERS));
+  myDatabase.ref("starters").child("alphabetized").child(randomIndex).once('value').then(snapshot =>
+  {
+      let letters = snapshot.val();
+      string = letters;
+      console.log(letters);
+
+      for(var i = 0; i < letters.length; i++){
+        $('letters').innerHTML += letters[i] + "       ";
+        if(i == 3 || i == 7)
+          $('letters').innerHTML += "<br>";
+      }
+  });
+}
+
+$('submit').addEventListener("click", function()
 {
-    let letters = snapshot.val();
-    string = letters;
-    console.log(letters);
-
-    for(var i = 0; i < letters.length; i++){
-      document.getElementById("letters").innerHTML += letters[i] + "      ";
-      if(i == 3 || i == 7)
-        document.getElementById("letters").innerHTML += "<br>";
-    }
-});
-  
-document.getElementById("submit").addEventListener("click", function(){
-  let guess = document.getElementById("guess").value;
+  let guess = $('guess').value;
   console.log(guess);
   checkGuess(guess);
 });
 
+$("start").addEventListener("click", function()
+{
+  startGame();
+});
+
+$("reset").addEventListener("click", function(){
+  endGame();
+});
+
 function checkGuess(guess){
+  let tmp = string        // used to determine if > # of correct letters are used
   for(var i = 0; i < guess.length; i++){
-    if(string.includes(guess[i])){
+    let ch = guess[i];
+    if(tmp.includes(ch)){
+      tmp = tmp.replace(ch, "");
+      console.log(tmp);
       continue;
     } else {
       alert("Invalid.  Please use only the letters on the board");
@@ -56,8 +98,8 @@ function checkGuess(guess){
     if(ss.exists() && !prevGuesses.includes(guess)){
       currentScore += guess.length;
       prevGuesses.push(guess);
-      document.getElementById("words").innerHTML += "-  " + guess + "<br>";
-      document.getElementById("score").innerHTML = "Score:  " + currentScore;
+      $('words').innerHTML += "-  " + guess + "<br>";
+      $('score').innerHTML = "Score:  " + currentScore;
       return true;
     } else if(prevGuesses.includes(guess)) {
       alert("Word already found!");
@@ -65,5 +107,16 @@ function checkGuess(guess){
       alert("Not a word!");
       return false;
     }
-  })  
+  })
+}
+
+let setUpTimer = function(){
+  if(seconds != 0){
+    seconds--;
+    $("time").innerHTML = "Time: " + seconds;
+  }else {
+    $("over").innerHTML = "GAME OVER" + "<br>" + "Final Score: " + currentScore;
+    $("guess").disabled = true;
+  }
+  console.log(seconds);
 }
